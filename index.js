4edb4cdae00e2e2aa03051d2d0587c61bb1f87d1
hamzaig <hamzaig@yahoo.com>
2022-12-10 15:42:51 +0500

    initial backend setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dbConnection = require('./config/database');
 require('./middleware/logger')();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
